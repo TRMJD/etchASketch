8506a5d1107a72136f45e893ec21e95a5bc74e58
TRMJD <joshua.defiesta@yahoo.com>
2024-01-07 10:53:59 -0800

    created 16x16 grid

--- java.js
+++ java.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+//create the 16x16 grid
+function createGrid(n) {
+    const middle = document.getElementsByClassName("middle");
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let x = 1; x <= n; x++) {
+            const cell = document.createElement("div");
+            cell.className = "gridsquares";
+            row.appendChild(gridsquare);
+        }
+        middle.appendChild(row);
+    }
+}
