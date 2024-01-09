c826b1f9205542fbea13f94021c858f74132f6cb
TRMJD <joshua.defiesta@yahoo.com>
2024-01-08 17:19:43 -0800

    added hover effect with grid

--- java.js
+++ java.js
@@ -1,16 +1,29 @@
 console.log("Hello World!");
 
 //create the 16x16 grid
-function createGrid(n) {
-    const middle = document.getElementsByClassName("middle");
-    for (let i = 0; i < n; i++) {
+function createGrid() {
+    const middle = document.getElementById("drawArea");
+    for (let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.className = "row";
-        for (let x = 1; x <= n; x++) {
+        for (let x = 1; x <= 16; x++) {
             const cell = document.createElement("div");
             cell.className = "gridsquares";
-            row.appendChild(gridsquare);
+            // cell.textContent = "hi";
+            cell.addEventListener('mouseover', (event) => {
+                cell.style.backgroundColor = 'red';
+            })
+            row.appendChild(cell);
         }
         middle.appendChild(row);
+        
     }
+    
 }
+
+//hover effect using entering and exiting event listeners
+// function changeColor() {
+//     cell.style.backgroundColor = "black";
+// }
+
+document.addEventListener('onload', createGrid());
