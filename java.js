20b66ff4071197602d298e507d3b9584632b1845
TRMJD <joshua.defiesta@yahoo.com>
2024-01-10 01:31:41 -0800

    started buttons for grid

--- java.js
+++ java.js
@@ -1,29 +1,32 @@
 console.log("Hello World!");
 
 //create the 16x16 grid
-function createGrid() {
+function createGrid(gridSizeNumber) {
     const middle = document.getElementById("drawArea");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSizeNumber; i++) {
         const row = document.createElement("div");
         row.className = "row";
-        for (let x = 1; x <= 16; x++) {
+        for (let x = 1; x <= gridSizeNumber; x++) {
             const cell = document.createElement("div");
             cell.className = "gridsquares";
-            // cell.textContent = "hi";
+            //hover effect 
             cell.addEventListener('mouseover', (event) => {
                 cell.style.backgroundColor = 'red';
             })
             row.appendChild(cell);
         }
         middle.appendChild(row);
-        
     }
-    
 }
 
-//hover effect using entering and exiting event listeners
-// function changeColor() {
-//     cell.style.backgroundColor = "black";
-// }
+//change grid size button
+const gridSizeBtn = document.getElementById("gridSizeBtn");
+gridSizeBtn.onclick = (event) => {
+    let gridSizeNumber = prompt("Enter the grid size number");
+    createGrid(gridSizeNumber);
 
-document.addEventListener('onload', createGrid());
+}
+
+
+
+document.addEventListener('onload', createGrid(gridSizeNumber = 16));
